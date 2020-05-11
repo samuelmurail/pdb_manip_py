93dc6d2cdf35457593c788d094c183270076a8c0
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2020-05-11 23:12:20 +0200

    Bump version: 1.1.2 → 1.2.0

--- pdb_manip_py/__init__.py
+++ pdb_manip_py/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Samuel Murail"
 __copyright__ = "Copyright 2020, RPBS"
 __credits__ = ["Samuel Murail"]
 __license__ = "GNU General Public License v2.0"
-__version__ = "__version__ = '1.1.2'"
+__version__ = "__version__ = '1.2.0'"
 __maintainer__ = "Samuel Murail"
 __email__ = "samuel.murail@u-paris.fr"
 __status__ = "Production"
